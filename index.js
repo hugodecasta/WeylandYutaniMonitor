8c66f1398b8e47c34931abd82fabed0712ba9375
hugodecasta <hugodecasta@orange.fr>
2020-11-28 13:00:02 +0100

    better mobile compatibility

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const favicon = require('serve-favicon')
 const port = process.env.PORT
 
+app.use(favicon(`${__dirname}/assets/favicon.png`))
 app.use('/assets', express.static(`${__dirname}/assets`));
 
 app.get('/', (req, res) => {
