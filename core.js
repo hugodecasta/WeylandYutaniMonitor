cdaeab230cee58af350bf4053f6bf96fce53b018
hugodecasta <hugodecasta@orange.fr>
2020-11-29 17:01:14 +0100

    beam system

--- core.js
+++ core.js
@@ -16,76 +16,42 @@ const DEBUG = process.env.DEBUG == 'true'
 
 let requests = {
     'version': async () => JSON.parse(await fs.promises.readFile(`${__dirname}/package.json`)).version,
-    'load_agent': async (agent) => {
-        agent = await load_agent(agent)
-        return agent
-    },
-    'load_sub_agent': async ({ agent, sub_agent }) => {
-        agent = await load_agent(agent)
-        sub_agent = await load_agent(sub_agent)
-        if (!agent) sub_agent = null
-        return sub_agent
-    },
-    'save_agent': async ({ agent, from_agent }) => {
-        let true_agent = await load_agent(agent)
-        if (!true_agent) return false
-        if (true_agent.n1) {
-            if (!from_agent) return false
-            let true_from = await load_agent(from_agent)
-            if (true_agent.n1 != true_from?.agent_code) return false
-        }
-        return await save_agent(agent)
-    },
-    'register_new_agent': async ({ from_agent, args }) => {
-        from_agent = await load_agent(from_agent)
-        if (!from_agent) return false
-        return await register_new_agent(from_agent, ...args)
-    },
-    'get_conversation': async (agent) => await get_conversation(agent),
-    'register_io': async (agent, socket) => {
-        let true_agent = await load_agent(agent)
-        if (!true_agent) return false
-        let secret = enc_secret(agent)
-        if (!agent_sockets[secret]) agent_sockets[secret] = []
-        agent_sockets[secret].push(socket)
-        return true
-    },
-    'report': async ({ agent, message }) => {
-        return await new_message(agent, agent.agent_code, 'report', message)
-    },
-    'message': async ({ agent, sub_agent, message }) => {
-        return await new_message(sub_agent, agent.agent_code, 'message', message)
+    'beam_secret': async ({ beam_name, beam_code }) => beam_secret(beam_name, beam_code),
+    'load_beam_file': async ({ beam_name, beam_code }) => await load_beam_file(beam_name, beam_secret(beam_name, beam_code)),
+    'send_message': async ({ beam_name, beam_code, personal_id, message }) => {
+        await send_to_beam(beam_name, beam_code, personal_id, message)
+        return await save_message(beam_name, beam_code, personal_id, message)
     }
 }
 
 // ------------------ INIT
 
-var agent_sockets = {}
+var all_sockets = []
 
 function init_io(io_engine) {
     io = io_engine
     io.on('connection', (socket) => {
-        let secret = null
+        all_sockets.push(socket)
         socket.on('disconnect', () => {
-            if (secret)
-                if (agent_sockets[secret])
-                    agent_sockets[secret].splice(agent_sockets[secret].indexOf(socket), 1)
+            all_sockets.splice(all_sockets.indexOf(socket), 1)
         })
         socket.on('request', async ({ name, data }) => {
-            if (name == 'register_io') secret = enc_secret(data)
             if (name in requests) {
-                setTimeout(async () => socket.emit('request_response', await requests[name](data, socket)), DEBUG ? 0 : response_to)
+                const response_to_here = (data && ('TO_OVERRIDE_DIV' in data)) ? response_to / data.TO_OVERRIDE_DIV : response_to
+                setTimeout(async () => socket.emit('request_response', await requests[name](data, socket)), DEBUG ? 0 : response_to_here)
+            }
+            else {
+                socket.emit('request_response', { error: 'request does not exist' })
             }
         })
     })
 }
 
-function send_to_agent(agent, name, data) {
-    let secret = enc_secret(agent)
-    if (!agent_sockets[secret]) return
-    agent_sockets[secret].forEach(socket => {
-        socket.emit(name + secret, data)
-    })
+// ------------------ SEND TO
+
+function send_to_beam(beam_name, beam_code, personal_id, message) {
+    let secret = beam_secret(beam_name, beam_code)
+    all_sockets.forEach(socket => socket.emit(secret, { date: Date.now(), personal_id, message }))
 }
 
 // ---------------------------------------------------------- BASE METHODS
@@ -109,69 +75,24 @@ async function save_file(name, secret, data) {
     return true
 }
 
-// ------------------ AGENT
-
-async function create_agent_object(n1_agent, firstname, lastname, birth, job, status) {
-    let code = Math.trunc(Math.random() * 10000)
-    let agent_code = (code + '').padStart(5, '0') + '-' + firstname.toUpperCase().charAt(0) + lastname.toUpperCase().charAt(0)
-    let agent_secret = DEBUG ? '00000' : (Math.trunc(Math.random() * 100000) + '')
-    let now = Date.now()
-    let new_agent = {
-        firstname, lastname,
-        code,
-        agent_code,
-        agent_secret,
-        birth,
-        job,
-        status,
-        last_read: now,
-        n1: null,
-        base: {
-            'subs': [],
-            'nodes': [],
-        }
-    }
-    if (n1_agent) {
-        n1_agent.base.subs.push({ agent_code, agent_secret, last_read: now })
-        new_agent.n1 = n1_agent.agent_code
-        await save_agent(n1_agent)
-    }
-    return new_agent
-}
-
-function enc_secret(agent) {
-    return `${agent.agent_code.toUpperCase()}${agent.agent_secret.toUpperCase()}`
-}
-
-async function save_agent(agent) {
-    send_to_agent(agent, 'save_agent', agent)
-    return await save_file(agent.agent_code, enc_secret(agent), agent)
-}
-
-async function load_agent(agent) {
-    return await load_file(agent.agent_code, enc_secret(agent))
-}
-
-async function register_new_agent(agent, firstname, lastname, birth, job, status) {
-    let reg_agent = await create_agent_object(agent, firstname, lastname, birth, job, status)
-    await save_agent(reg_agent)
-    if (agent) send_to_agent(agent, 'sub_registered', reg_agent)
-    return reg_agent
+async function load_beam_file(beam_name, secret) {
+    return await load_file(beam_name, secret) ?? []
 }
 
 // ------------------ MESSAGES
 
-async function get_conversation(agent) {
-    return (await load_file(`${agent.agent_code}${agent.agent_secret}conv`, enc_secret(agent))) ?? []
+function beam_secret(beam_name, beam_code) {
+    return beam_name + beam_code + '_beam'
 }
 
-async function new_message(agent, author, type, message) {
-    let conversation = await get_conversation(agent)
-    let message_data = { date: Date.now(), author, type, message }
-    conversation.push(message_data)
-    send_to_agent(agent, 'new_' + type, message_data)
-    return await save_file(`${agent.agent_code}${agent.agent_secret}conv`, enc_secret(agent), conversation)
+async function save_message(beam_name, beam_code, personal_id, message) {
+    let secret = beam_secret(beam_name, beam_code)
+    let beam_file = await load_beam_file(beam_name, secret)
+    let message_data = { date: Date.now(), personal_id, message }
+    beam_file.push(message_data)
+    return await save_file(beam_name, secret, beam_file)
 }
+
 
 // ---------------------------------------------------------- INIT
 
@@ -179,11 +100,9 @@ async function core_init(io_engine) {
     init_io(io_engine)
     if (!fs.existsSync(base_dir)) {
         fs.mkdirSync(base_dir)
-        let pweyland = await register_new_agent(null, 'PETER', 'WEYLAND', '01/10/1990', 'CEO', 'ALIVE')
-        console.log('CREATED FIRST AGENT:', pweyland)
     }
 }
 
 // ---------------------------------------------------------- EXPORTS
 
-module.exports = { core_init, new_message, get_conversation, register_new_agent, load_agent, save_agent }
+module.exports = { core_init }
