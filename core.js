6a9f2bef2e54753f1c9fe20de7686f7d15fa4dce
hugodecasta <hugodecasta@orange.fr>
2020-11-30 22:57:57 +0100

    better beam unread messages checker

--- core.js
+++ core.js
@@ -7,8 +7,10 @@ const fs = require('fs')
 
 const base_dir = `${__dirname}/base`
 var io = null
-const response_to = 2000
 const DEBUG = process.env.DEBUG == 'true'
+const response_to = DEBUG ? 0 : 2000
+
+const system_beam_ids = { beam_name: 'SYS-BEAM', beam_code: process.env.SYS_BEAM_CODE, personal_id: 'BEAM-SYSTEM' }
 
 // ---------------------------------------------------------- IO SYSTEM
 
@@ -18,6 +20,10 @@ let requests = {
     'version': async () => JSON.parse(await fs.promises.readFile(`${__dirname}/package.json`)).version,
     'beam_secret': async ({ beam_name, beam_code }) => beam_secret(beam_name, beam_code),
     'load_beam_file': async ({ beam_name, beam_code }) => await load_beam_file(beam_name, beam_secret(beam_name, beam_code)),
+    'load_beam_new_messages': async ({ beam_name, beam_code, start_date }) =>
+        (await load_beam_file(beam_name, beam_secret(beam_name, beam_code)))
+            .filter(message_data => message_data.date > start_date),
+    'search_in_beam': async ({ beam_name, beam_code, query }) => await search_in_beam(beam_name, beam_code, query),
     'send_message': async ({ beam_name, beam_code, personal_id, message }) => {
         let message_data = await save_message(beam_name, beam_code, personal_id, message)
         await send_to_beam(beam_name, beam_code, personal_id, message, message_data.date)
@@ -38,14 +44,27 @@ function init_io(io_engine) {
         })
         socket.on('request', async ({ name, data, response_code }) => {
             if (name in requests) {
-                const response_to_here = (data && ('TO_OVERRIDE_DIV' in data)) ? response_to / data.TO_OVERRIDE_DIV : response_to
-                setTimeout(async () => socket.emit('request_response_' + response_code, await requests[name](data, socket)), DEBUG ? 0 : response_to_here)
+                const begin = Date.now()
+                const response = await requests[name](data, socket)
+                const end = Date.now()
+                const time_spent = end - begin
+                const remaining_time = response_to - time_spent
+                const time_to_send = Math.max(0, remaining_time)
+                setTimeout(async () => socket.emit('request_response_' + response_code, response), time_to_send)
             }
             else {
                 socket.emit('request_response_' + response_code, { error: 'request does not exist' })
             }
         })
     })
+}
+
+// ------------------ SYSTEM LOG
+
+function sys_log(message) {
+    let { beam_name, beam_code, personal_id } = system_beam_ids
+    send_to_beam(beam_name, beam_code, personal_id, message, Date.now())
+    save_message(beam_name, beam_code, personal_id, message, true)
 }
 
 // ------------------ SEND TO
@@ -86,15 +105,26 @@ function beam_secret(beam_name, beam_code) {
     return beam_name + beam_code + '_beam'
 }
 
-async function save_message(beam_name, beam_code, personal_id, message) {
+async function save_message(beam_name, beam_code, personal_id, message, prevent_log = false) {
     let secret = beam_secret(beam_name, beam_code)
     let beam_file = await load_beam_file(beam_name, secret)
     let message_data = { date: Date.now(), personal_id, message }
     beam_file.push(message_data)
     await save_file(beam_name, secret, beam_file)
+    if (!prevent_log)
+        sys_log(`${personal_id} saved message\n\n${message}\n\non beam ${beam_name} (${beam_code}) at ${message_data.date}`)
     return message_data
 }
 
+// ------------------ SEARCH ENGINE
+
+async function search_in_beam(beam_name, beam_code, query) {
+    let secret = beam_secret(beam_name, beam_code)
+    let beam_file = await load_beam_file(beam_name, secret)
+    let found_messages = beam_file.map(({ date, personal_id, message }) => {
+        let str = `date:${date};personal id:${personal_id};${message}`
+    })
+}
 
 // ---------------------------------------------------------- INIT
 
